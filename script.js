c432f8e79facc45b8b872b9927740843ea44e1c1
cooper <mail@coopersimpson.me>
2022-03-12 18:26:48 +1000

    Finished logic for playRound function

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 function computerPlay() {
   let options = ["Rock", "Paper", "Scissors"];
-  console.log(options);
   const randomElement = options[Math.floor(Math.random() * options.length)];
-  console.log(options);
-  console.log(randomElement);
   return randomElement;
 }
 
-// function playRound(playerSelection, computerSelection) {
-//     if playerSelection == computerSelection return
+// Set computer selection
+let computerSelection = computerPlay();
+
+// Player chooses selection
+let choice = prompt("Enter: Rock Paper or Scissors");
+let playerSelection =
+  choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
+
+console.log(playerSelection);
+console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    console.log(`You win ${playerSelection} beats ${computerSelection}`);
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    console.log(`You lose ${computerSelection} beats ${playerSelection}`);
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    console.log(`You win ${playerSelection} beats ${computerSelection}`);
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    console.log(`You lose ${computerSelection} beats ${playerSelection}`);
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    console.log(`You win ${playerSelection} beats ${computerSelection}`);
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    console.log(`You lose ${computerSelection} beats ${playerSelection}`);
+  } else {
+    console.log(`Its a tie, ${playerSelection} ties with ${computerSelection}`);
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
+// //loop with a function that calls playRound, game of 5
+
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     playRound(playerSelection, computerSelection);
+//   }
 // }
 
-// const computerSelection = computerPlay();
-// const playerSelection = "rock";
+// how to prompt input on each loop through?
